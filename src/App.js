0e9fc61a869ae5d783426a073d6da2790d223fbb
pashyaa6255 <76003615+pashyaa6255@users.noreply.github.com>
2021-04-23 22:51:42 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const api = `https://randomuser.me/api`;
+
+  const [user, setUser] = useState("Nothing Inside, Its Empty Here");
+
+  const addUserHandler = async () => {
+    const userData = await fetch(api, {
+      method: "GET"
+    });
+    const userJson = await userData.json();
+    console.log(userJson);
+    setUser(userJson);
+  };
+  return (
+    <div>
+      <button onClick={addUserHandler}>ADD User</button>
+      <div>{user}</div>
+      <input></input>
+    </div>
+  );
+}
